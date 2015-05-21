391d54a2df955f66ac5432b907642153db0d7d77
Ali Ghorbani <ali.ghorbani@servicenow.com>
2015-05-21 01:04:44 -0700

    Added the data file generation code

---
+++ CreateDioctionary.py
@@ -0,0 +1,48 @@
+
+import xml.etree.ElementTree as ET
+from bs4 import BeautifulSoup
+from nltk.corpus import stopwords
+from nltk.tokenize import RegexpTokenizer
+from nltk.stem.porter import *
+import pickle
+import re
+
+_digits = re.compile('\d')
+dictionary_file_name = "dictionary.p"
+stemmer = PorterStemmer()
+
+def build_dictionary(texts, all_words):
+    tokenizer = RegexpTokenizer(r'\w+')
+    for w in texts:
+        word_list = [stemmer.stem(word.lower()) for word in tokenizer.tokenize(w)]
+        word_list = [w for w in word_list if w not in stopwords.words('english')]
+        for word in word_list:
+            if not bool(_digits.search(word)):
+                all_words.add(word)
+
+    pass
+
+def load_file_content(file_name):
+    all_words = set()
+    e = ET.parse(file_name).getroot()
+    for atype in e.findall('item'):
+        contents = atype.find('contents/value')
+
+        if contents is not None:
+            soup = BeautifulSoup(contents.text)
+            texts = soup.findAll(text=True)
+            build_dictionary(texts, all_words)
+
+    pickle.dump(all_words, open(dictionary_file_name, "wb"))
+    print all_words
+    print len(all_words) # 31669
+
+
+def verify_dictionary():
+    all_words2 = pickle.load(open(dictionary_file_name, "rb"))
+    print all_words2
+    print len(all_words2)
+    pass
+
+# load_file_content("NewYorkTimes.xml")
+verify_dictionary()
--- ProcessContent.py
+++ ProcessContent.py
@@ -1,46 +1,83 @@
 
 import xml.etree.ElementTree as ET
-from scrapy import Item, Field
 from bs4 import BeautifulSoup
-import sys
+from nltk.corpus import stopwords
+from nltk.tokenize import RegexpTokenizer
+from nltk.stem.porter import *
+import pickle
+import re
 
-class ArticleEntry(Item):
-    issue = Field()
-    title = Field()
-    author = Field()
-    contents = Field()
+_digits = re.compile('\d')
+dictionary_file_name = "dictionary.p"
+articles_file_name = "articles.p"
+stemmer = PorterStemmer()
 
-all_words = set()
+class ArticleEntry:
+    title = ""
+    writer = ""
+    publication = ""
+    dict_0_1 = {}
+    dict_count = {}
 
-def build_dictionary(texts):
+    def __init__(self, title, writer, publication):
+        self.title = title
+        self.writer = writer
+        self.publication = publication
+
+    def __unicode__(self):
+        return u"{0}, {1}, {2}".format(self.title, self.writer, self.publication)
+
+    def __str__(self):
+        return unicode(self).encode('utf-8')
+
+def built_article_dictionary(the_dictionary, article, texts):
+    tokenizer = RegexpTokenizer(r'\w+')
+    article.dict_0_1 = dict.fromkeys(the_dictionary, 0)
+    article.dict_count = dict.fromkeys(the_dictionary, 0)
+
     for w in texts:
-        all_words.add(w)
-
+        word_list = [stemmer.stem(word.lower()) for word in tokenizer.tokenize(w)]
+        for word in word_list:
+            if word in the_dictionary:
+                article.dict_0_1[word] = 1
+                article.dict_count[word] += 1
     pass
 
-def load_file_content(file_name):
+def process_articles(file_name):
+    the_dictionary = load_dictionary();
+    articles = []
     e = ET.parse(file_name).getroot()
     for atype in e.findall('item'):
+        contents = atype.find('contents/value')
         issue = atype.find('issue/value')
         title = atype.find('title/value')
         author = atype.find('author/value')
-        contents = atype.find('contents/value')
 
-        # if issue is not None:
-        #     print issue.text
-        #
-        # if title is not None:
-        #     print title.text
+        if issue is not None and title is not None and author is not None and contents is not None:
+            article = ArticleEntry(title.text, author.text, issue.text)
 
-        # if author is not None:
-        #     print author.text
-
-        if contents is not None:
             soup = BeautifulSoup(contents.text)
             texts = soup.findAll(text=True)
-            build_dictionary(texts)
+            built_article_dictionary(the_dictionary, article, texts)
+
+            articles.append(article)
+
+    print len(articles)
+    pickle.dump(articles, open(articles_file_name, "wb"))
+    print "done"
 
 
-load_file_content("NewYorkTimes.xml")
-print all_words
-print len(all_words)
+def load_dictionary():
+    all_words = pickle.load(open(dictionary_file_name, "rb"))
+    print len(all_words)
+    return all_words
+
+def test_articles():
+    articles = pickle.load(open(articles_file_name, "rb"))
+    print len(articles) # 182
+    print articles[5] # Hong Kong's Umbrella Revolution Isn't Over Yet, LAUREN HILGERS, FEB. 18, 2015
+    print articles[5].dict_0_1['hi'] # 1
+    print articles[5].dict_count['hi'] # 23
+
+# process_articles("NewYorkTimes.xml")
+test_articles()
